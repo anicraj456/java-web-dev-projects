3a5f72ad222d1d11e1c9869a948139c111477415
anicraj456 <anicraj123@gmail.com>
2023-11-29 17:33:57 -0600

    studio completed

---
+++ datatypes/datatypes-studio/src/main/java/Area.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Area {
+        public static void main(String[] args){
+            double radius;
+            double area;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the radius of the circle ::");
+            radius = input.nextDouble();
+             area = Circle.getArea(radius);
+            System.out.println("Area of the circle is: " +area);
+        }
+    }
