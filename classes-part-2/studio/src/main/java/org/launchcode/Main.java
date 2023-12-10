d39bb29af4c64b9339738e83ec502a341d475c72
anicraj456 <anicraj123@gmail.com>
2023-12-10 15:04:58 -0600

    working on studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -3,6 +3,14 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
+        MenuItem item1 = new MenuItem("Samosa","stuffed savory pastry", 2.25, "appetizer");
+        MenuItem item2 = new MenuItem("Chicen 65","soft and juicy chicken", 9.75, "appetizer");
+        MenuItem item3 = new MenuItem( "Briyani","Aromatic chicken rice", 13.99, "main course");
+        MenuItem item4 = new MenuItem("Butter masala","Creamy chicken masala", 12.99,"main course");
+        MenuItem item5 = new MenuItem("Jamun","sweet balls", 1.25, "dessert");
+
+        //System.out.println(item1.isNew());
+        System.out.println(item3);
         // write your code here
     }
 }
--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,19 +1,81 @@
 package org.launchcode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
-    private boolean isNew;
+    private LocalDate dateAdded;
+    //private boolean isNew;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
+
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.dateAdded = LocalDate.now();
+        //this.isNew = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        String newText = isNew() ?  " -NEW!" : "";
+        return name  + newText + "\n" + description  + "--$" + price ;
+    }
+
+    boolean isNew(){
+        LocalDate today = LocalDate.now();
+        double daysBtw = getDateAdded().until(today, ChronoUnit.DAYS);
+        return daysBtw < 90;
+    }
+
+}
+
+
+
+    /*public void setPrice(double price) {
         this.price = price;
     }
 
@@ -27,6 +89,6 @@ public class MenuItem {
 
     public void setNew(boolean aNew) {
         isNew = aNew;
-    }
-}
+    }*/
+
 
