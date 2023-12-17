523e5dd0b3a12d37f57b6cba9864d1a9db79cf9e
anicraj456 <anicraj123@gmail.com>
2023-12-16 21:02:58 -0600

    chap exercise

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -22,10 +22,13 @@ class CarTest {
     public void testInitialGasTank(){
         //Car test_car = new Car("Toyota", "Prius", 10, 50);
         assertEquals(10,test_car.getGasTankLevel(), .001);
+
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
 
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+
     //TODO: can't have more gas than tank size, expect an exception
 }
