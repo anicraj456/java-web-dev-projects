a9fa2b536c42ece0768ccaabb055ffcfa1cae7fb
anicraj456 <anicraj123@gmail.com>
2024-01-08 20:15:25 -0600

    completed

--- controllers-and-routing/studio/controllers-routing-studio/src/main/java/org/launchcode/controllers/SkillsController.java
+++ controllers-and-routing/studio/controllers-routing-studio/src/main/java/org/launchcode/controllers/SkillsController.java
@@ -1,64 +1,74 @@
-package org.launchcode.controllers;
+package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class SkillsController {
-
-    @RequestMapping(value="")
-    @ResponseBody
-    public String startSkills () {
-        String html = "<h1>Skills Tracker</h1>" +
-                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+    //Handles localhost:8080/home
+    @GetMapping("home")
+    public String homePage(){
+        return "<html>" +
+                "<body>" +
+                "<h1> Skills Tracker </h1>" +
+                "<h2> We have a few skills we would like to learn.Here is the list!" +
                 "<ol>" +
-                "<li>Java</li>" +
-                "<li>JavaScript</li>" +
-                "<li>Python</li>" +
-                "</ol>";
-
-        return html;
+                "<li> Java </li>" +
+                "<li> JavaScript </li>" +
+                "<li> Python </li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
     }
-
-    @GetMapping(value="form")
-    @ResponseBody
-    public String formSkills() {
-        String html = "<form method='post'>" +
-                "Name:<br>" +
-                "<input type = 'text' name = 'name' />" +
-                "<br>My favorite language:<br>" +
-                "<select name = 'firstChoice'>" +
-                "<option value = 'Java'>Java</option>" +
-                "<option value = 'Javascript'>JavaScript</option>" +
-                "<option value = 'Python'>Python</option>" +
-                "</select>" +
-                "<br>My second favorite language:<br>" +
-                "<select name = 'secondChoice'>" +
-                "<option value = 'Java'>Java</option>" +
-                "<option value = 'Javascript'>JavaScript</option>" +
-                "<option value = 'Python'>Python</option>" +
-                "</select>" +
-                "<br>My third favorite language:<br>" +
-                "<select name = 'thirdChoice'>" +
-                "<option value = 'Java'>Java</option>" +
-                "<option value = 'Javascript'>JavaScript</option>" +
-                "<option value = 'Python'>Python</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit' />" +
-                "</form>";
-
-        return html;
+    //handles localhost:8080/home/form
+    @GetMapping("form")
+    public String displayPage() {
+        return "<html>" +
+                "<body>" +
+                "<form action='form' method='POST'>" +
+                "<label>Name</label>" +
+                "<input type='text' name='name' /><br/>" +
+                "<label> My Favorite Language </label>" +
+                "<select name = 'fav1'>" +
+                "<option value='Java'> Java </option>" +
+                "<option value='JavaScript'> JavaScript </option>" +
+                "<option value='Python'> Python </option>" +
+                "</select><br/>" +
+                "<label> My Second Favorite Language </label>" +
+                "<select name = 'fav2'>" +
+                "<option value='Java'> Java </option>" +
+                "<option value='JavaScript'> JavaScript </option>" +
+                "<option value='Python'> Python </option>" +
+                "</select><br/>" +
+                "<label> My Third Favorite Language </label>" +
+                "<select name = 'fav3'>" +
+                "<option value='Java'> Java </option>" +
+                "<option value='JavaScript'> JavaScript </option>" +
+                "<option value='Python'> Python </option>" +
+                "</select><br/>" +
+                "<input type = 'submit' value = 'submit' />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
-
-    @PostMapping(value="form")
-    @ResponseBody
-    public String namePage(@RequestParam String name, @RequestParam String firstChoice, @RequestParam String secondChoice, @RequestParam String thirdChoice) {
-        String html = "<h1>" + name + "</h1>" +
-                "<ol>" +
-                "<li>" + firstChoice + "</li>" +
-                "<li>" + secondChoice + "</li>" +
-                "<li>" + thirdChoice + "</li>" +
-                "</ol>";
+    //handles localhost:8080/home/form postmapping
+    @PostMapping("form")
+    public String namePage(@RequestParam String name, @RequestParam String fav1, @RequestParam String fav2, @RequestParam String fav3) {
+        String html =
+                "<html>" +
+                        "<body>" +
+                        "<h1>" + name + "</h1>" +
+                        "<ol>" +
+                        "<li>" + fav1 + "</li>" +
+                        "<li>" + fav2 + "</li>" +
+                        "<li>" + fav3 + "</li>" +
+                        "</ol>" +
+                        "</body>"+
+                        "</html>";
         return html;
     }
 }
